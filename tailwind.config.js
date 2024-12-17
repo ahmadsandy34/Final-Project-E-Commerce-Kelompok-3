196810bd801970fb326ce5881d1e2f4b75771e35
Sandy Arib Ahmad <ahmadsandyarib@gmail.com>
2024-12-17 16:20:38 +0700

    feat: add contact page
    add contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content:  ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "custom-light": "0px 1px 13px 0px #0000000D",
+      },
+    },
   },
   plugins: [],
-}
-
+};
